96dabbb29389bbcd6fcd62eedace95c880b0805d
Mahmoud Saada <mahmoudsaada@gmail.com>
2020-05-23 12:34:45 -0400

    complete frontend

--- frontend/pages/api.js
+++ frontend/pages/api.js
@@ -1,6 +1,18 @@
-export default (req, res) => {
-    // TODO: make request to localhost:8080
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ name: 'John Doe' }))
+import fetch from 'isomorphic-unfetch'
+
+const API_URL = "http://localhost:8080/test";
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const r = await fetch(API_URL, {
+      method: "POST",
+      body: JSON.stringify(req.body),
+      headers: { "Content-Type": "application/json" },
+    });
+    let type = r.status == 200 ? "success" : "error"
+    let message = await r.json();
+    res.status(r.status).json({ type, message });
+  } else {
+    res.status(405).json({ type: "error", message: "Must be a POST request" });
   }
+};
