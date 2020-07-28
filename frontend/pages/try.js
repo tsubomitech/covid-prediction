d4349b496dff7d2d47a319357b120970c131288b
Mahmoud Saada <mahmoudsaada@gmail.com>
2020-07-28 09:30:48 -0400

    update nextjs, materialui, remove fieldset, bring back required checks

--- frontend/pages/try.js
+++ frontend/pages/try.js
@@ -145,109 +145,90 @@ export default function Try() {
   if (response.type === "error") {
     output = <div style={{ color: "red" }}>{response.message}</div>;
   } else if (response.message) {
-
-    output = (
-      <Results payload={response.message}/>
-    );
+    output = <Results payload={response.message} />;
   }
   const classes = useStyles();
   return (
     <Layout>
       <h1>Prediction API</h1>
-      <p>Enter patient metrics below and the API will attempt to predict the patient's chance of survival</p>
+      <p>
+        Enter patient metrics below and the API will attempt to predict the
+        patient's chance of survival
+      </p>
       <form action="{API_URL}" method="post" onSubmit={handleSubmit}>
-        <fieldset id="forms__input">
-          <legend>input parameters</legend>
-          {/* <p>
-            <label for="input__text">Name</label>
-            <input
-              id="input__text"
-              type="text"
-              placeholder="Name"
-              onChange={handleChange}
-              required
-            />
-          </p> */}
-
-          {Object.keys(formFields).map((f) => (
-            <TextField
-              key={f}
-              id={"input__" + f}
-              name={f}
-              label={f}
-              type="number"
-              step="0.01"
-              value={formFields[f]}
-              onChange={handleChange}
-              style={{ margin: "5px", width: "auto" }}
-              // helperText="Enter a number"
-            />
-          ))}
-          <br />
-          <FormControl className={classes.formControl}>
-            <InputLabel id="models-label">Models</InputLabel>
-            <Select
-              labelId="models-label"
-              id="models-chip"
-              disabled={isSelectBoxLoading}
-              multiple
-              value={models.selected}
-              onChange={(e) =>
-                setModels({ ...models, selected: e.target.value })
-              }
-              input={<Input id="select-multiple-models" />}
-              renderValue={(selected) => (
-                <div className={classes.chips}>
-                  {models.selected.map((value) => (
-                    <Chip
-                      key={"chip__" + value}
-                      label={value}
-                      color="secondary"
-                      className={classes.chip}
-                    />
-                  ))}
-                </div>
-              )}
-              MenuProps={MenuProps}
-            >
-              {models.list.map((model) => (
-                <MenuItem
-                  key={"item__" + model}
-                  value={model}
-                  color="secondary"
-                >
-                  {model}
-                </MenuItem>
-              ))}
-            </Select>
-            {isSelectBoxLoading ? <CircularProgress color="secondary" /> : null}
-          </FormControl>
-          <FormControlLabel
-            control={
-              <Checkbox
-                required
-                checked={disclaimerChecked}
-                onChange={() => setDisclaimerChecked(!disclaimerChecked)}
-                name="disclaimerChecked"
-                color="secondary"
-              />
-            }
-            label="I understand that this app is only for educational purpose and not for diagnosis."
+        {Object.keys(formFields).map((f) => (
+          <TextField
+            key={f}
+            id={"input__" + f}
+            required
+            name={f}
+            label={f}
+            type="number"
+            step="0.01"
+            value={formFields[f]}
+            onChange={handleChange}
+            style={{ margin: "5px", width: "auto" }}
+            // helperText="Enter a number"
           />
-          <br />
-          <Button
-            type="submit"
-            size="large"
-            startIcon={<ArrowUpwardIcon />}
-            variant="contained"
-            color="primary"
-            disabled={isFormSubmitLoading}
+        ))}
+        <br />
+        <FormControl className={classes.formControl}>
+          <InputLabel id="models-label">Models</InputLabel>
+          <Select
+            labelId="models-label"
+            id="models-chip"
+            disabled={isSelectBoxLoading}
+            multiple
+            value={models.selected}
+            onChange={(e) => setModels({ ...models, selected: e.target.value })}
+            input={<Input id="select-multiple-models" />}
+            renderValue={(selected) => (
+              <div className={classes.chips}>
+                {models.selected.map((value) => (
+                  <Chip
+                    key={"chip__" + value}
+                    label={value}
+                    color="secondary"
+                    className={classes.chip}
+                  />
+                ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
           >
-            Submit
-          </Button>
-          {isFormSubmitLoading ? <CircularProgress color="primary" /> : null}
-          <div>{output}</div>
-        </fieldset>
+            {models.list.map((model) => (
+              <MenuItem key={"item__" + model} value={model} color="secondary">
+                {model}
+              </MenuItem>
+            ))}
+          </Select>
+          {isSelectBoxLoading ? <CircularProgress color="secondary" /> : null}
+        </FormControl>
+        <FormControlLabel
+          control={
+            <Checkbox
+              required
+              checked={disclaimerChecked}
+              onChange={() => setDisclaimerChecked(!disclaimerChecked)}
+              name="disclaimerChecked"
+              color="secondary"
+            />
+          }
+          label="I understand that this app is only for educational purpose and not for diagnosis."
+        />
+        <br />
+        <Button
+          type="submit"
+          size="large"
+          startIcon={<ArrowUpwardIcon />}
+          variant="contained"
+          color="primary"
+          disabled={isFormSubmitLoading}
+        >
+          Submit
+        </Button>
+        {isFormSubmitLoading ? <CircularProgress color="primary" /> : null}
+        <div>{output}</div>
       </form>
     </Layout>
   );
