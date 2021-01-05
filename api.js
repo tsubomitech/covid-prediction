2d705b30321312c946e36e90c12e7c3b09b4a14a
Kikemura7 <kikemura7@gmail.com>
2021-01-05 17:12:01 -0500

    Update api.js

--- api.js
+++ api.js
@@ -3,52 +3,52 @@ import auth from "basic-auth";
 
 const API_URL = process.env.API_URL || "http://localhost:8080";
 
-export default async (req, res) => {
-  //  var user = auth(req);
-  //  if (user?.name !== "setHeader" || user?.pass !== "K3nji") {
-  //    res.setHeader("WWW-Authenticate", 'Basic realm="private"');
-  //    res.status(401);
-  //    res.json({});
-  //    return;
-  //  }
+// export default async (req, res) => {
+//    var user = auth(req);
+//    if (user?.name !== "setHeader" || user?.pass !== "K3nji") {
+//      res.setHeader("WWW-Authenticate", 'Basic realm="private"');
+//      res.status(401);
+//      res.json({});
+//      return;
+//    }
 
-  const token = process.env.GOOGLE_CLOUD_RUN ? await getGoogleToken() : null;
-  if (req.method === "GET") {
-    const r = await fetch(API_URL + "/models", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    let type = r.status == 200 ? "success" : "error";
-    try {
-      let message = await r.json();
-      res.status(r.status).json({ type, message });
-    } catch (e) {
-      console.error(r.text());
-      console.error(e);
-      res.status(r.status).json({ type, message: "Failed to parse json" });
-    }
-  } else if (req.method === "POST") {
-    const r = await fetch(API_URL + "/test", {
-      method: "POST",
-      body: JSON.stringify(req.body),
-      headers: {
-        "x-model": req.headers["x-model"],
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    let type = r.status == 200 ? "success" : "error";
-    let message = await r.json();
-    res.status(r.status).json({ type, message });
-  } else {
-    res
-      .status(405)
-      .json({ type: "error", message: "Must be a GET or POST request" });
-  }
-};
+//   const token = process.env.GOOGLE_CLOUD_RUN ? await getGoogleToken() : null;
+//   if (req.method === "GET") {
+//     const r = await fetch(API_URL + "/models", {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     let type = r.status == 200 ? "success" : "error";
+//     try {
+//       let message = await r.json();
+//       res.status(r.status).json({ type, message });
+//     } catch (e) {
+//       console.error(r.text());
+//       console.error(e);
+//       res.status(r.status).json({ type, message: "Failed to parse json" });
+//     }
+//   } else if (req.method === "POST") {
+//     const r = await fetch(API_URL + "/test", {
+//       method: "POST",
+//       body: JSON.stringify(req.body),
+//       headers: {
+//         "x-model": req.headers["x-model"],
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     let type = r.status == 200 ? "success" : "error";
+//     let message = await r.json();
+//     res.status(r.status).json({ type, message });
+//   } else {
+//     res
+//       .status(405)
+//       .json({ type: "error", message: "Must be a GET or POST request" });
+//   }
+// };
 
 async function getGoogleToken() {
   const metadataServerTokenURL =
