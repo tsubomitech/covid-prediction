75b838a58e246910d6c6ab110e81145cfc5a6a02
Kikemura7 <kikemura7@gmail.com>
2021-01-05 16:46:41 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: {
-    NEXT_PUBLIC_APP_NAME: "Please contact Kenji Ikemura for password :)",
+    NEXT_PUBLIC_APP_NAME: "Tsubomi Technology - Montefiore Medical Center",
   }
 };
