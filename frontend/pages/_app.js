5c717b87acecd5db3bcae742707473a3a9fb7b76
Mahmoud Saada <mahmoudsaada@gmail.com>
2024-05-30 00:10:30 -0400

    deploy ui

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../components/theme';
-import Header from '../components/header';
+import React from "react";
+import PropTypes from "prop-types";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../components/theme";
+import Header from "../components/header";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -20,7 +20,6 @@ export default function MyApp(props) {
     <React.Fragment>
       <Header></Header>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
